16839eed92e1560257377ed0843945d3d0a3930a
sanny gupta <sannygupta79@gmail.com>
2023-04-26 23:13:24 +0530

    phir se same

--- demo1.py
+++ demo1.py
@@ -1,3 +1,46 @@
 this is sanny gupta 
 sjsk sdjfgiojs
 sdjgkj
+
+#   YE ADD KAISE KARNA HAI FILE KO GIT KE HELP SE GIT-BUSH ME   THESE ARE STEPS -----------------------------------
+
+# 1)  sabse phale folder me chala jana hai then git bish open karna hai
+# 2)  phir git status check karna hai then here show it's in MAIN  so convert MASTER function
+# 3)  using this COMMAND (git branch -M MASTER )
+# 4)  phir GITHUB walw repostatiry me PUSH kar denge using "git remote add origin https://github.com/SANNY-GUPTA/demo-github.git" in GIT-BUSH
+# 5)  phir se MASTER ko MAIN me change kar denge "git branch -M main"
+# 6)  phir se PUSH command ko RUN karaenge (git push -u origin main) then side me connect to "GIT HUB" open hoga there  use token or sign-in to enter the that page
+# 7)  
+
+
+
+#  AB ADD KARNE KE BAD KUCH FILE KO KAISE KO  CONTROL AND MANAGE KARNA HAI 
+# THESE ARE STEPS ----------------------------------------------
+
+# 1) SABSE PHALE GIT KO init KARNA THEN git status CHECK KARNA  THEN  "git add *"  karna 
+# 2) AGAR KUCH DELETE KARNA HO TO  "create ignore file"  usse phale git reset all file (git reset) COMMAND RUN KARANA PADEGA 
+# 3) phir (touch .gitignore) KO RUN KARANA PADEGA  THEN "ls" COMMAND KO 
+# 4)  check for it's its work or not means command work or not so (LS -LA in small) here show ".gitignore"  then it's OK
+# 5)  then "vi .gitignore" ko karaenge for kis-kis ko delete karna karna hia uske lea
+# 6)  step 5 me insert kara ke delete kara denge then click a) ESC  ,then b) :wq then   c) ckick  SHIFT + ENTER KO SAVE AND EXIT for that table 
+# 7)  AT LASAT me check git status for ko hamne 5 step me delete kea the usko dekhne ke lea ki delete hua hai ki nahi   
+
+
+
+
+#  DUSRE KE FILE OR APANE JO FILE GIT ME HAI USSE KE UPPER KAM KARNA  and  update karna HO TO  STEPS-----------------------------------------
+
+#  1) OPEN CMD TERMINAL AND CALL "git clone that link in HTTP:/" in CMD AND run that  line  
+# 2) aur jana gaya hai waha ja kar cmd me run karana   and  KUCH JO V ADD KARNA HO O KARNA  (matlab ki new or old me he kam karna) 
+# 3)  then git status check karna  AND BRANCH KO  git-hub wale se same karna and then again run kaarna 
+# 4) then ADD karna (git add .) command chala kar ke 
+# 5) then commit karna (git commit  -m "koi v name se save karna") then status again check karna 
+# 6)  MOST AND VERY IMPORTANT (PUSH KARNA) USKE LEA (git push origin main) ye command bahut hai to update hai nahi to nahi 
+# 7) last me git-hub me ja kar check karna ki file\folder\data save hua ki nahi
+#    
+
+
+
+# AGAR MAIN BRANCH ME KOI UPDATE KARNA HUA TO  DIRECT KARNE SE CONFLICT HO JAIEGA  SO (branching) KARNA PADEGA
+# A) SABSE PHALE   MAIN AND MASTER KO CONVERT KARKE KISSE AUR ME KAM KARNA PADEGA USSKE LEA (git checkout -b "fea_abc" jo name rakhna hai o rakh lenge)
+#  B) AFTER RUN THAT CODE SHOW (switched to a new branch 'fea_abc' name ) 
